b4c4cc7573202a751ad0eb82dc7010d5611db71b
Saharsh1223 <88390577+Saharsh1223@users.noreply.github.com>
2022-02-22 14:59:48 +0530

    Create main.py
    Watch the full tutorial on how to hide the token

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.command()
+async def hello(ctx):
+  await ctx.send("Hello!")
+
+token = os.environ['TOKEN']
+bot.run(token)
