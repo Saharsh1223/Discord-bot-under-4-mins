d8d073f9cfb5a8937d689b089435e330af01865c
Saharsh1223 <88390577+Saharsh1223@users.noreply.github.com>
2022-12-20 07:10:45 +0530

    Update

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!", intent=discord.Intents.default())
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.command()
 async def hello(ctx):
